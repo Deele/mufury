d384c38384d3b2625935b5c99e69e57284f71dac
work4itjobs <work4itjobs@gmail.com>
2015-07-12 22:34:14 +0300

    navigation widget updated as per design draft

--- draft/build/build_less.sh
+++ draft/build/build_less.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 lessc dist/css/main.less --source-map=dist/css/main.less.map --source-map-basepath=/vagrant/dist --source-map-rootpath=/ --source-map-url=/css/main.less.map --clean-css > dist/css/main.css
--- draft/vagrant_bootstrap.sh
+++ draft/vagrant_bootstrap.sh
@@ -5,9 +5,9 @@ apt-get -qy install apache2 nodejs npm git
 ln -s /usr/bin/nodejs /usr/bin/node
 npm config set strict-ssl false
 npm config set loglevel warn
-#npm install -g bower less less-plugin-clean-css
+npm install -g bower less less-plugin-clean-css
 
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant/dist /var/www
+if ! [ -L /var/www/html ]; then
+  rm -rf /var/www/html
+  ln -fs /vagrant/dist /var/www/html
 fi
